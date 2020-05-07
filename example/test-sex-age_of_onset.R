6b5b2cfb6d25d26184af2828a9da4749da94e062
Bjarni Vilhjalmsson <bjarni.vilhjalmsson@gmail.com>
2020-05-07 15:39:18 +0200

    Almost finished implementing new approach

--- example/test-sex-age_of_onset.R
+++ example/test-sex-age_of_onset.R
@@ -1,31 +1,78 @@
-set.seed(1)
-h2 <- 0.5
-N <- 5e4
-NA_frac = 1/3
+set.seed(42)
 
-#max_prev is between [0,1]
-#ages is defined between [0,1]
-#years is defined between [0,1]
-#sex is 1 or 2
-#sex_eff is defined between [0,1]
-get_prev <- function(max_prev,ages,years,sex,sex_eff=1){
-  prev = ages*years*max_prev
-  prev[sex==2]*sex_eff
-  return(prev)
-}
+#Loading necessary libraries
+library(MASS)
+library(tidyverse)
+library(dplyr)
+
+# 1. Simulate cumulative incidence rates
+#    a) By sex, age, and year
+#
+# 2. Simulate sample
+#    a) Simulate liabilities
+#    b) Simulate age, sex, year
+#
+# 3. For each sampled individual identify case status and age-of-onset
+#
+############ -- This concludes the input data simulation -- #############
+# 
+# 4. For each genotyped individual identify case-control status identify integral region
+#    a) Group individuals by 
+#         - age (for controls) 
+#         - age-of-onset (for cases)
+#         - year
+#         - sex
+#    b) Determine prevalences in each group
+#    c) For each individual identify integral thresholds based on group
+#         - Note that a case in age-of-onset group 2 will be integrated between 2 and 1.
+#
+# 5. Simulate liabilities and estimate posterior means
+#    a) Simulate liabilities
+#    b) Identify possible combinations of thresholds (presumably thousands) and create an ordered list 
+#         - e.g. ordered by first threshold in each dimension
+#    c) For each simulated liability identify groups that it is in and add genetic liability
+#    d) Calculate average liabilities in each group
+#    e) If not enough simulations, then repeat 4 (or use importance sampling).
+#
+# 6. For each sampled individual, look up the relevant posterior mean.
+
+#Some basic constants
+h2 <- 0.5
+max_prev <- 0.5
+sex_eff <- 0.5
+all_ages <- seq(100)
+all_years <- c(1920:2019)
+all_sex <- c(1,2)
+
+# 1. Simulate cumulative incidence rates
 
 #Can be updated for a non-normal distribution, say using observed simulated distribution.
 get_thres <- function(prev){
-    thr <- qnorm(1 - prev)
-    return(thr)
+  thr <- qnorm(1 - prev)
+  return(thr)
+}
+get_prev <- function(ages,years,sex){
+  prev = ((ages-min(all_ages))/length(all_ages))*((years-min(all_years))/length(all_years))*max_prev
+  prev[sex==2] = prev[sex==2]*sex_eff
+  return(prev)
 }
 
-get_cc_stat <- function(liab, thr){
-  cc_status = liab>thr
-  return(cc_status)
+get_thres_table <- function(){
+  d <-  expand_grid(age=all_ages, year=all_years, sex=all_sex)
+  d <- filter(d,age<2020-year) # Age is limited by year of birth
+  prev_list = get_prev(d['age'],d['year'],d['sex'])
+  d <- cbind(d, prev_list)
+  names(d)[4] <- 'prev'
+  d <- cbind(d, get_thres(d[,4]))
+  names(d)[5] <- 'thres'
+  return(d)
 }
 
+thres_table <- get_thres_table()
 
+filter(thres_table,age=40,year=1980)
+
+# 2. Simulate sample
 #### Simulating some data (liabilities) ####
 simTrioLiab = function(N, h2) {
   gCov = diag(h2, 3)
@@ -46,12 +93,213 @@ simTrioLiab = function(N, h2) {
   fLiab <- fGenLiab + fEnvLiab 
   mLiab <- mGenLiab + mEnvLiab 
   cLiab <- cGenLiab + cEnvLiab 
-  trio_liabs = tibble(child_gen = cGenLiab, child_full = cLiab, mother_gen = mGenLiab, mother_full=mLiab, father_gen = fGenLiab, father_full=fLiab)
+
+  c_sex <- sample(1:2, N, replace = TRUE)
+  c_age <- sample(1:35, N, replace = TRUE)
+  c_year <- sample(1981:2010, N, replace = TRUE)
+  m_sex <- rep(1,N)
+  m_age <- sample(20:80, N, replace = TRUE)
+  m_year <- sample(1950:1990, N, replace = TRUE)
+  f_sex <- rep(2,N)
+  f_age <- sample(20:80, N, replace = TRUE)
+  f_year <- sample(1950:1990, N, replace = TRUE)
+  
+  trio_liabs = tibble(iid = seq(N), child_gen = cGenLiab, child_full = cLiab, child_sex = c_sex, child_age = c_age, child_year=c_year,
+                      mother_gen = mGenLiab, mother_full=mLiab, mother_sex = m_sex, mother_age = m_age, mother_year=m_year,
+                      father_gen = fGenLiab, father_full=fLiab, father_sex = f_sex, father_age = f_age, father_year=f_year)
   return(trio_liabs)
 }
 
-trio_liabs <- simTrioLiab(N,h2)
+trio_liabs <- simTrioLiab(1e5,h2)
+# Age is limited by year of birth, etc
+trio_liabs <- filter(trio_liabs,child_age<2020-child_year, mother_age<2020-mother_year, father_age<2020-father_year, child_age<mother_age-15, child_age<father_age-15) 
+trio_liabs
+#Filter impossible combinations
 
+# 3. For each sampled individual identify case status and age-of-onset
+
+get_age_of_onset <- function(l,y,s){
+  t = filter(thres_table,year==y,sex==s,thres<=l)
+  if (nrow(t)==0){
+    return (NA)
+  }
+  else{
+    return(min(t['age']))
+  }
+}
+get_ao <- Vectorize(get_age_of_onset)
+
+get_eff_age <- function(age,age_of_onset){
+  if (is.na(age_of_onset)){
+    return (age)
+  }
+  else{
+    return(age_of_onset)
+  }
+}
+get_ea <- Vectorize(get_eff_age)
+
+child_info <- trio_liabs %>% 
+  left_join(thres_table,by = c("child_age" = "age", 'child_year'='year', 'child_sex'='sex')) %>% 
+  select(-starts_with('father'))%>% 
+  select(-starts_with('mother'))%>%
+  mutate(child_status=as.integer(child_full>thres))%>%
+  mutate(child_age_of_onset=get_ao(child_full,child_year,child_sex))%>% #Not very efficient
+  mutate(child_eff_age=get_ea(child_age,child_age_of_onset))%>%
+  rename(child_thres=thres,child_prev=prev)
+
+t <- filter(child_info,child_status==0)
+print(t)
+
+mother_info <- trio_liabs %>% 
+  left_join(thres_table,by = c("mother_age" = "age", 'mother_year'='year', 'mother_sex'='sex')) %>% 
+  select(-starts_with('father'))%>% 
+  select(-starts_with('child'))%>% 
+  mutate(mother_status=as.integer(mother_full>thres))%>% 
+  mutate(mother_age_of_onset=get_ao(mother_full,mother_year,mother_sex))%>% #Not very efficient
+  mutate(mother_eff_age=get_ea(mother_age,mother_age_of_onset))%>%
+  rename(mother_thres=thres,mother_prev=prev)
+
+filter(mother_info,mother_status==1)
+
+father_info <- trio_liabs %>% 
+  left_join(thres_table,by = c("father_age" = "age", 'father_year'='year', 'father_sex'='sex')) %>% 
+  select(-starts_with('mother'))%>% 
+  select(-starts_with('child'))%>%
+  mutate(father_status=as.integer(father_full>thres))%>%
+  mutate(father_age_of_onset=get_ao(father_full,father_year,father_sex))%>% #Not very efficient
+  mutate(father_eff_age=get_ea(father_age,father_age_of_onset))%>%
+  rename(father_thres=thres,father_prev=prev)
+father_info
+filter(father_info,father_status==1)
+
+#Now combine into a large table summarizing all information simulated
+sample_table = inner_join(child_info,mother_info,by='iid')
+sample_table = inner_join(sample_table,father_info,by='iid')
+
+############ -- This concludes the input data simulation -- #############
+# 
+# 4. For each genotyped individual identify case-control status identify integral region
+#    a) Group individuals by 
+#         - age (for controls) 
+#         - age-of-onset (for cases)
+#         - year
+#         - sex
+
+year_group_thres <- c(1990,2000)
+year_groups <- seq(length(year_group_thres)+1)
+age_group_thres <- c(30,60)
+age_groups <- seq(length(age_group_thres)+1)
+sex_groups <- c(1,2)
+
+group_table = expand_grid(year_group=year_groups,age_group=age_groups,sex_group=sex_groups)
+group_table
+
+map_2_age_groups <- function(v){
+  return(sum(v>age_group_thres)+1)
+}
+m2ag <- Vectorize(map_2_age_groups)
+
+map_2_year_groups <- function(v){
+  return(sum(v>year_group_thres)+1)
+}
+m2yg <- Vectorize(map_2_year_groups)
+
+sample_table_ext <-sample_table %>% 
+  mutate(child_age_group=m2ag(child_eff_age))%>%
+  mutate(mother_age_group=m2ag(mother_eff_age))%>%
+  mutate(father_age_group=m2ag(father_eff_age))%>%
+  mutate(child_year_group=m2yg(child_year))%>%
+  mutate(mother_year_group=m2yg(mother_year))%>%
+  mutate(father_year_group=m2yg(father_year))
+sample_table_ext
+
+#    b) Determine prevalences in each group
+##Get observed prevalences and thresholds for each group
+thres_table <- thres_table %>% 
+  mutate(age_group=m2ag(age))%>%
+  mutate(year_group=m2yg(year))%>%
+  group_by(age_group,year_group,sex)%>%
+  mutate(
+    group_prev = mean(prev),
+  ) %>%
+  mutate(
+    group_thres = qnorm(1 - group_prev),
+  )
+
+thres_groups <- thres_table %>% 
+  select(sex,age_group,year_group,group_prev,group_thres)%>%
+  distinct()
+
+#    c) For each individual identify integral thresholds based on group
+#         - Note that a case in age-of-onset group 2 will be integrated between 2 and 1.
+# Loop over all individuals
+
+get_int_interval <- function(status,ag,yg,sg){
+  group_thres <- filter(thres_groups,age_group==ag,year_group==yg,sex==sg)[[1,'group_thres']]
+  if(status==0){
+    lbound <- -Inf
+    ubound <-group_thres
+  }
+  else{
+    lbound <-group_thres
+    cond_age_groups = filter(thres_groups,year_group==yg,sex==sg)['age_group']
+    if(ag==max(cond_age_groups)){
+      ubound <- Inf
+    }
+    else{
+      ubound <- filter(thres_groups,age_group==ag+1,year_group==yg,sex==sg)[[1,'group_thres']]
+    }
+  }
+  return (list('lbound'=lbound,'ubound'=ubound))
+}
+
+
+clbound = c()
+cubound = c()
+mlbound = c()
+mubound = c()
+flbound = c()
+fubound = c()
+for (i in 1:nrow(sample_table_ext)){
+  #child_thresholds
+  cstatus = sample_table_ext[[i,'child_status']]
+  cag = sample_table_ext[[i,'child_age_group']]
+  cyg = sample_table_ext[[i,'child_year_group']]
+  csg = sample_table_ext[[i,'child_sex']]
+  l = get_int_interval(cstatus,cag,cyg,csg)
+  clbound[i] = l$lbound
+  cubound[i] = l$ubound
+
+  mstatus = sample_table_ext[[i,'mother_status']]
+  mag = sample_table_ext[[i,'mother_age_group']]
+  myg = sample_table_ext[[i,'mother_year_group']]
+  msg = sample_table_ext[[i,'mother_sex']]
+  l = get_int_interval(mstatus,mag,myg,msg)
+  mlbound[i] = l$lbound
+  mubound[i] = l$ubound
+
+  fstatus = sample_table_ext[[i,'father_status']]
+  fag = sample_table_ext[[i,'father_age_group']]
+  fyg = sample_table_ext[[i,'father_year_group']]
+  fsg = sample_table_ext[[i,'father_sex']]
+  l = get_int_interval(fstatus,fag,fyg,fsg)
+  flbound[i] = l$lbound
+  fubound[i] = l$ubound
+}
+
+int_interval_table = tibble(clbound,cubound,mlbound,mubound,flbound,fubound)
+
+sample_table_ext<- cbind(sample_table_ext,int_interval_table)
+
+
+# 5. Simulate liabilities and estimate posterior means
+#    a) Simulate liabilities
+#    b) Identify possible combinations of thresholds (presumably thousands) and create an ordered list 
+#         - e.g. ordered by first threshold in each dimension
+#    c) For each simulated liability identify groups that it is in and add genetic liability
+#    d) Calculate average liabilities in each group
+#    e) If not enough simulations, then repeat 4 (or use importance sampling).
 
 #### Simulate multivariate liabilities ####
 covSim <- diag(c(h2, 1 - h2, 1, 1))
@@ -59,92 +307,32 @@ colnames(covSim) <- rownames(covSim) <- c("child_gen", "child_env", "father_full
 covSim[3:4, 1] <- covSim[1, 3:4] <- h2 / 2  
 
 #Only need to simulate liabilites once.  We can reuse for different values of K
-simu_liab <- mvtnorm::rmvnorm(2e7, sigma = covSim)  
+N_sim = 5e3
+simu_liab <- mvtnorm::rmvnorm(N_sim, sigma = covSim)  
+config_table = distinct(int_interval_table)
+config_table <-config_table %>%
+  add_column(liab_sum = rep(0,nrow(config_table)))%>%
+  add_column(num_hits = rep(0,nrow(config_table)))
+config_table
 
-#### Now simulate age, year of birth, sex, etc. ####
-
-# Sampling age, year and sex
-c_sex <- sample(1:2, N, replace = TRUE)
-c_age <- sample(1:2, N, replace = TRUE)/2
-c_year <- sample(1:2, N, replace = TRUE)/2
-m_sex <- rep(1,N)
-m_age <- sample(1:2, N, replace = TRUE)/2
-m_year <- sample(1:2, N, replace = TRUE)/2
-f_sex <- rep(2,N)
-f_age <- sample(1:2, N, replace = TRUE)/2
-f_year <- sample(1:2, N, replace = TRUE)/2
-
-#Now get prevelances
-max_prev = 0.5
-c_prev = get_prev(max_prev,c_age,c_year,c_sex,sex_eff=0.5)
-m_prev = get_prev(max_prev,m_age,m_year,m_sex,sex_eff=0.5)
-f_prev = get_prev(max_prev,f_age,f_year,f_sex,sex_eff=0.5)
-
-#Now get thresholds
-c_thres = get_thres(c_prev)
-m_thres = get_thres(m_prev)
-f_thres = get_thres(f_prev)
-
-#Now get case-control status
-child_status <- get_cc_stat(trio_liabs['child_full'],c_thres)
-mother_status <- get_cc_stat(trio_liabs['mother_full'],m_thres)
-father_status <- get_cc_stat(trio_liabs['father_full'],f_thres)
-
-#Now figure out all possible configurations based on thresholds/prevalences
-nb_var <- ncol(cov) - 1
-all_config <- do.call(expand.grid, c(rep(list(c(NA,1:(nrow(K)*2))), nb_var), list(1:2))) ### change
-names(all_config) <- c("child_status", "father_status", "mother_status", "child_sex")
-all_config$string <- as.factor(do.call(paste, all_config + 0L))
-all_config
-
-
-
-for (i in 2:ncol(cov)) { #starting at 2 to not filter on os twice, i.e. environment and genetic filtering
-  simu_liab[base::sample(1:nrow(simu_liab), nrow(simu_liab) * NA_frac),i] <- NA
+for (i in 1:nrow(simu_liab)){
+  cl <- simu_liab[i,1]+simu_liab[i,2]
+  ml <- simu_liab[i,3]
+  fl <- simu_liab[i,4]
+  for (j in 1:nrow(config_table)){
+    in_reg <- cl>config_table[[j,1]] & cl<config_table[[j,2]] & ml>=config_table[[j,3]] & ml<config_table[[j,4]] & fl>=config_table[[j,5]] & fl<config_table[[j,6]]
+    if (in_reg){
+      config_table[[j,7]] = config_table[[j,7]]+simu_liab[i,1]
+      config_table[[j,8]] = config_table[[j,8]]+1
+    }
+  }
+  print (config_table[,'num_hits'])
 }
-
-
-library(dplyr)
-df_simu_liab <- tibble(
-  child_gen     = simu_liab[, 1],
-  child_sex     = sample(1:2, nrow(simu_liab), replace = TRUE),
-  child_status  = assign_status_sex(simu_liab[, 1] + simu_liab[, 2], thr, child_sex),
-  father_status = assign_status(simu_liab[, 3], thr[,1]),
-  mother_status = assign_status(simu_liab[, 4], thr[,2])
-) %>%
-  left_join(all_config) %>%
-  print()
-
-
-group_means <- group_by(df_simu_liab, string, .drop = FALSE) %>%
-  summarise(post_mean_liab = mean(child_gen), n = n(),
-            se = sd(child_gen) / sqrt(n)) %>%
-  ungroup() %>%
-  arrange(post_mean_liab) %>%
-  print()
 
 
 #### Assign group posterior mean genetic liabilities to individuals ####
 
-child_group <- tibble(child_gen, child_status, father_status, mother_status, child_sex) %>%
-  sample_frac(downsample_frac, weight = ifelse(child_status > nrow(thr), 1e6, 1)) %>%
-  left_join(all_config) %>%
-  left_join(group_means) %>%
-  print()
-count(child_group, child_status, child_sex)
-count(child_group, child_status, "GWAS" = (child_status > nrow(thr)) + 0L)
-
-trues = child_group %>% group_by(string) %>% summarise(true_mean_liab = mean(child_gen), n_true = n(), true_se = sd(child_gen) / sqrt(n_true)) %>% left_join(group_means)
-
-library(ggplot2)
-ggplot(child_group) +
-  bigstatsr::theme_bigstatsr() + 
-  geom_point(aes(post_mean_liab, child_gen, 
-                 color = as.factor(child_sex)), alpha = 0.3) + 
-  #  geom_point(data = trues, aes(post_mean_liab, true_mean_liab)) +
-  geom_abline(col = "black") + 
-  theme(legend.position = "top")
-with(child_group, c(cor(post_mean_liab, child_gen), cor(child_status > nrow(K), child_gen)))
+# Plot
 
 ####/w sex + age, 50% cases
 #prev <- c(0.08, 0.02) + N = 10k      0.7161538 0.6231014
