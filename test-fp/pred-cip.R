9e5c1c2bbaafaec1b05d9898061e0516fb837015
Florian Privé <florian.prive.21@gmail.com>
2020-05-22 09:39:50 +0200

    cip

--- test-fp/pred-cip.R
+++ test-fp/pred-cip.R
@@ -19,7 +19,8 @@ cip %>%
 
 
 cip %>%
-  filter(dx == "adhd") %>%
+  filter(dx == "D33") %>%
+  filter(birth_year %% 5 == 0) %>%
   ggplot(aes(age, cip, color = sex)) +
   geom_line() +
   facet_wrap(~ birth_year, scales = "free_y")
@@ -44,12 +45,14 @@ xgb <- xgboost(X, y, nrounds = 200, params = bst.params)
 xgboost::xgb.importance(model = xgb)
 
 
-params <- expand.grid(age = 0:120, birth_year = 1930:2015, sex = c("K", "M"))
-params$cip_pred <- pmax(predict(xgb, bigstatsr::covar_from_df(params)), 1e-6)
+params <- expand.grid(age = 0:120, birth_year = 1930:2015, sex = c("F", "M"))
+params$cip_pred <- pmax(predict(xgb, bigstatsr::covar_from_df(params)), 1e-4)
 params <- params %>%
   group_by(birth_year, sex) %>%
   arrange(age) %>%
-  mutate(cip_pred = cummax(cip_pred))
+  mutate(cip_pred = cummax(cip_pred), 
+         thr = qnorm(cip_pred, lower.tail = FALSE))
+params[-4]  # would be provided to the user
 
 
 bigstatsr::plot_grid(
